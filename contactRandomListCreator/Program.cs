d6a3ab47e2eabc9d0d4c94cc90f1e17463557a34
Vitaliy Belyakov <MoNaX_667@mail.ru>
2016-05-13 00:07:07 +0300

    RandomContactListCreator
    test

---
+++ contactRandomListCreator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace contactRandomListCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "ContactList.txt";
+            RandomContactListCreator creator = new RandomContactListCreator();
+
+            File.WriteAllLines(filename, creator.CreateContactList(100));
+
+            Console.Write("Creating ... secceed.... press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ contactRandomListCreator/Sex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace contactRandomListCreator
+{
+    enum Sex
+    {
+        Male,
+        Female,
+    }
+}
